c4c24b8ef6b3a0fa238329d9c7b9b9b935183c15
rogelisdev <rogelisdev@gmail.com>
2026-05-29 12:32:09 -0500

    Correccion ejercicio 2

--- segundo-ejercicio.js
+++ segundo-ejercicio.js
@@ -1,24 +1,23 @@
-function validarContrasena(contrasena){
-    if(contrasena.length <= 8){
+function validarContrasena(contrasena) {
+    if (contrasena.length < 8) {
         console.log("Necesitas una contraseña con mas de 8 caracteres, intente denuevo")
         return false;
-    } else{
+    }
+
+    if (!/[A-Z]/.test(contrasena)) {
+        console.log("Debe incluir al menos una mayúscula");
+        return false;
+    }
+
+    if (!/[0-9]/.test(contrasena)) {
+        console.log("Debe incluir al menos una numero");
+        return false;
+    }
+    console.log("Contraseña aceptada")
     return true;
-    }
 
-    if(typeof contrasena !== "string"){return false;
-    return /[a-z]/.test(contrasena);
-    console.log("Tu contraseña incluye una minuscula")
 }
 
-    if(typeof contrasena !== "string"){return false;
-    return /[A-Z]/.test(contrasena);
-
-    console.log("Tu contraseña incluye una mayuscula")
-    }
-
-    console.log("Tu contraseña cumple con todos los requisitos")
-}
 
 
 
